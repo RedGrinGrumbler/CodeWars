5bfbedd65691083847b3255ded42d4e30ca281af
Adam C Smith <aRustyDev@users.noreply.github.com>
2021-04-05 09:50:53 -0500

    Passed!
    Had an issue where I was comparing the arr[i]<arr[i-1] instead of
    arr[i]<arr[low]

--- C/8-kyu/find-the-smallest-integer-in-the-array.c
+++ C/8-kyu/find-the-smallest-integer-in-the-array.c
@@ -2,5 +2,11 @@
 
 int find_smallest_int(int *vec, size_t len)
 {
-    return *vec;
+    int low=0;
+    for(size_t i=1; i<len; i++){
+      if( (int)vec[i]<(int)vec[low] ){
+        low = i;
+      }
+    }
+    return vec[low];
 }
