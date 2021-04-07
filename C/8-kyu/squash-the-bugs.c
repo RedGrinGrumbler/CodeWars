d0e6820f13435fa9dbe1cdec820ad6cff39f5e8e
Adam C Smith <36318507+aRustyDev@users.noreply.github.com>
2021-04-07 16:54:26 -0500

    Passed!
    There was a bug that prevented the last word from being the largest word

--- C/8-kyu/squash-the-bugs.c
+++ C/8-kyu/squash-the-bugs.c
@@ -1,8 +1,15 @@
+//::Simple challenge::
+//Eliminate all bugs from the supplied code so that the code runs and outputs the expected value. 
+//Output should be the length of the longest word, as a number.
+//
+//There will only be one 'longest' word.
+
 #include <stddef.h>
 #include <ctype.h>
 
 size_t find_longest(const char *words) {
-  size_t length = 0, max_length = 0;
+  size_t length = 0;
+  size_t max_length = 0;
   const char *pchar = words;
   
   char c;
@@ -15,6 +22,9 @@ size_t find_longest(const char *words) {
     } else {
       length++;
     }
+  }
+  if (length > max_length) {
+    max_length = length;
   }
   return max_length;
 }
