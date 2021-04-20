65ef5188de18071d530e28397978fe50e802ceb9
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-20 04:39:23 -0500

    Update which-are-in.lua

--- Lua/6-kyu/which-are-in.lua
+++ Lua/6-kyu/which-are-in.lua
@@ -2,8 +2,19 @@ local solution = {}
 
 function solution.inArray(array1, array2)
     solution = {}
+    print("============")
+    print("----arr1----")
+    for i1,k1 in ipairs(array1) do
+      print(k1)
+    end
+    print("----arr2----")
+    for i1,k1 in ipairs(array2) do
+      print(k1)
+    end
+    --print(string.gsub("abcd.?.efg", "(%.)", "%%."))
     for i1,k1 in ipairs(array1) do
       s="("..table.concat(solution, ')(')..")"
+      k1:gsub("(%.)", "%%.")
       ptrn="[^%a]"..k1.."[^%a]"
       for i2,k2 in ipairs(array2) do  
         if (string.find(k2, k1)) and (string.find(s, ptrn)==nil)
@@ -13,7 +24,8 @@ function solution.inArray(array1, array2)
         end
       end
     end
-    return table.sort(solution)
+    table.sort(solution)
+    return solution
 end
   
 return solution
