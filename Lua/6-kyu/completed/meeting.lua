15c9cd9d8b4ec3687bef52e03951b03dcee2c6e2
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-19 22:58:21 -0500

    Passed!
    Had a heck of a time, I think theres an issue with how they wrote this challenge.   
    The table wasn't being cleared before starting, so any data from the previous run was left over & returned alongside the new data.

--- Lua/6-kyu/completed/meeting.lua
+++ Lua/6-kyu/completed/meeting.lua
@@ -1,16 +1,12 @@
 local solution = {}
 
 function solution.meeting(s)
+    solution = {}
     for f,l in string.gmatch(s,"(%a+):(%a+)") do
       table.insert(solution, string.upper("("..l..", "..f..")"))
     end
     table.sort(solution)
-    local out_str=""
-    for i,line in ipairs(solution) do
-      out_str=out_str..line
-      --print(out_str)
-    end 
-    return out_str
+    return table.concat(solution)
 end
   
 return solution
