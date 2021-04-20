d8c6393686bae7b07e9a9e12333dd528ec9c7ca8
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-19 23:08:06 -0500

    added comments

--- Lua/6-kyu/completed/meeting.lua
+++ Lua/6-kyu/completed/meeting.lua
@@ -1,9 +1,9 @@
 local solution = {}
 
 function solution.meeting(s)
-    solution = {}
-    for f,l in string.gmatch(s,"(%a+):(%a+)") do
-      table.insert(solution, string.upper("("..l..", "..f..")"))
+    solution = {} --If this isnt here the previous data remains
+    for f,l in string.gmatch(s,"(%a+):(%a+)") do --grep for Fname & Lname
+      table.insert(solution, string.upper("("..l..", "..f..")")) --arrange & format the name
     end
     table.sort(solution)
     return table.concat(solution)
