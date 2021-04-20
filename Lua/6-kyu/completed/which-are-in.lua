3658c25e67146a4fe331b77f3ed156dbc764b006
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-20 05:07:34 -0500

    Update which-are-in.lua

--- Lua/6-kyu/completed/which-are-in.lua
+++ Lua/6-kyu/completed/which-are-in.lua
@@ -19,3 +19,25 @@ function solution.inArray(array1, array2)
 end
   
 return solution
+
+--============================================================================
+--ALTERNATE
+local solution = {}
+
+function solution.inArray(array1, array2)
+    local found, res = {}, {}
+    for _, a1 in ipairs(array1) do 
+        for _, a2 in ipairs(array2) do
+            if string.find(a2, a1, 1, true) then -- and found[a1] == nil then
+                found[a1] = true
+            end
+        end
+    end
+    for i, _ in pairs(found) do
+        table.insert(res, i)
+    end
+    table.sort(res)
+    return res
+end
+
+return solution
