8408da605c0f7025042d26f11fb3ac450fbfc717
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-20 00:47:18 -0500

    Update counting-sheep.c

--- C/8-kyu/completed/counting-sheep.c
+++ C/8-kyu/completed/counting-sheep.c
@@ -2,7 +2,6 @@
 #include <stddef.h>
 
 size_t count_sheep(const bool *sheep, size_t total){
-    printf("size: %d\n", total);
     size_t sleep = 0;
     for(int i = 0; i < total; i++){
         if(true==sheep[i]){
