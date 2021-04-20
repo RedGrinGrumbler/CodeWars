17c47d8f6a37ab3ece3b7a0421a6f7990591ce23
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-04-20 05:03:22 -0500

    PASSED MF!!!
    Bejeezes this took waaaay to freaking long to figure out. 
    I had it down until the "." that was being passed to my string.find() as an 'any character' which triggered one stupid freaking erroneous match. 
    But I freaking got it!!
    I spent so damn long trying to get the damn ptrn to fucking work but I fucking got it.

--- Lua/6-kyu/which-are-in.lua
+++ Lua/6-kyu/which-are-in.lua
@@ -2,19 +2,9 @@ local solution = {}
 
 function solution.inArray(array1, array2)
     solution = {}
-    print("============")
-    print("----arr1----")
-    for i1,k1 in ipairs(array1) do
-      print(k1)
-    end
-    print("----arr2----")
-    for i1,k1 in ipairs(array2) do
-      print(k1)
-    end
-    --print(string.gsub("abcd.?.efg", "(%.)", "%%."))
     for i1,k1 in ipairs(array1) do
       s="("..table.concat(solution, ')(')..")"
-      k1:gsub("(%.)", "%%.")
+      k1=k1:gsub("(%.)", "%%.")
       ptrn="[^%a]"..k1.."[^%a]"
       for i2,k2 in ipairs(array2) do  
         if (string.find(k2, k1)) and (string.find(s, ptrn)==nil)
