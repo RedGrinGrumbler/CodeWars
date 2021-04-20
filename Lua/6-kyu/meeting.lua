b4908347bbd793dc6f8cb9d9082e20108427b99e
Adam C Smith <aRustyDev@users.noreply.github.com>
2021-04-19 19:48:40 -0500

    Still testing

--- Lua/6-kyu/meeting.lua
+++ Lua/6-kyu/meeting.lua
@@ -1,9 +1,13 @@
 local solution = {}
 
 function solution.meeting(s)
-    --match names
     for i in string.gmatch(s,"%a+:%a+") do
-      print(i)
+      local x = i:gsub("(%a+):(%a+)", "%2:%1")
+      local y = x:find(":")-1
+      print(x)
+      print("Last: "..x:sub(1,y))
+      print("First: "..x:sub(y,x:len(x)))
+      print("")
     end
     return ""
 end
